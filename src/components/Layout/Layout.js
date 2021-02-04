845721191e45c9d31cd3474867f7cf1f9f4b1e81
Kofi Boakye <66253143+boakyezitek@users.noreply.github.com>
2021-02-04 15:17:50 +0000

    full app done with theme switcher to dark or light mode.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,42 @@
-import React from "react";
 import Head from "next/head";
-import styles from "../Layout/Layout.module.css";
-import Link from  "next/link";
-function Layout({children}) {
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Brightness6Rounded } from "@material-ui/icons";
+import styles from "./Layout.module.css";
+
+const Layout = ({ children, title = "World Ranks" }) => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      localStorage.getItem("theme")
+    );
+
+    setTheme(localStorage.getItem("theme"));
+  }, []);
+
+  const switchTheme = () => {
+    if (theme === "light") {
+      saveTheme("dark");
+    } else {
+      saveTheme("light");
+    }
+  };
+
+  const saveTheme = (theme) => {
+    setTheme(theme);
+    localStorage.setItem("theme", theme);
+    document.documentElement.setAttribute("data-theme", theme);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <header className={styles.header}>
         <Link href="/">
           <svg
@@ -38,14 +66,16 @@ function Layout({children}) {
           </svg>
         </Link>
 
-        {/* <button className={styles.themeSwitcher} onClick={switchTheme}>
+        <button className={styles.themeSwitcher} onClick={switchTheme}>
           <Brightness6Rounded />
-        </button> */}
+        </button>
       </header>
+
       <main className={styles.main}>{children}</main>
-      <footer className={styles.footer}>Text footer</footer>
+
+      <footer className={styles.footer}>Tonacode @ tonacode.com</footer>
     </div>
   );
-}
+};
 
 export default Layout;
